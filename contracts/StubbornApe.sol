ed67a810110f7d4d6a244596a86e0e10de503eab
shahfaisal-brilliant <Faisal.farooq@brilliantgamez.com>
2022-02-04 15:43:08 +0500

    Admin Screen UI

---
+++ contracts/StubbornApe.sol
@@ -0,0 +1,167 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract StubbornApe is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+    using SafeMath for uint256;
+
+    string public baseURI;
+
+    // Costs ====================================
+    uint256 public presaleCost = 0.02 ether;
+    uint256 public publicSaleCost = 0.08 ether;
+
+    // Sale dates ===============================
+    uint public presaleStartTimestamp = 1649959200; // 14 April 2022 5 PM EST
+    uint public publicSaleStartTimestamp = 1650114000; // 16 April 2022 12 PM EST
+
+    // Count values =============================
+    uint256 public MAX_ITEMS = 10000;
+    uint256 public _mintedItems = 0;
+    uint256 public maxMintAmount = 10; // Max items per tx
+
+    bool public paused = false;
+
+    // Lists ====================================
+    mapping(address => bool) public whitelisted;
+    mapping(address => uint) public presaleWallets;
+
+    // Events ===================================
+    event PublicSaleMint(
+        address indexed _from,
+        uint indexed _tokenId
+    );
+
+    constructor(
+        string memory _initBaseURI
+    ) ERC721('Stubborn Apes', 'APE') {
+        setBaseURI(_initBaseURI);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // Presale Minting ============================
+     function presaleMint() public payable {
+        require(!paused, "Contract is paused!");
+        require(block.timestamp >= presaleStartTimestamp, "Presale is not opened yet");
+        require(block.timestamp <= publicSaleStartTimestamp, "Not in presale period");
+        uint remainder = msg.value % presaleCost;
+        uint _mintAmount = msg.value / presaleCost;
+        require(remainder == 0, "Send a divisible amount of price");
+        require(_mintedItems.add(_mintAmount) <= MAX_ITEMS, "Purchase would not exceed max supply");
+        require(_mintAmount <= maxMintAmount, "Over the maxMintAmount");
+        require(whitelisted[msg.sender] == true, "You need to be in whitelist");
+        require(presaleWallets[msg.sender] <= 10, "Max 10 items per wallet");
+
+        if (msg.sender != owner()) {
+            for (uint i = 0; i < _mintAmount; i++) {
+                uint mintIndex = _mintedItems;
+                require(_mintedItems <= MAX_ITEMS, "All items sold!");
+                _safeMint(msg.sender, mintIndex);
+                _mintedItems++;
+            }
+            presaleUser(msg.sender, presaleWallets[msg.sender] + _mintAmount);
+        }
+    }
+
+    // Public sale Minting =================================
+     function publicSaleMint() public payable {
+        require(!paused, "Contract is paused");
+        require(block.timestamp >= publicSaleStartTimestamp, "Public sale is not opened yet");
+        uint remainder = msg.value % publicSaleCost;
+        uint _mintAmount = msg.value / publicSaleCost;
+        require(remainder == 0, "Send a divisible amount of price");
+        require(_mintedItems.add(_mintAmount) <= MAX_ITEMS, "Purchase would  not exceed max supply of Basic Apes");
+        require(_mintAmount <= maxMintAmount, "Over the maxMintAmount");
+
+        if (msg.sender != owner()) {
+            for (uint i = 0; i < _mintAmount; i++) {
+                uint mintIndex = _mintedItems;
+                require(_mintedItems <= MAX_ITEMS, "All items sold!");
+                _safeMint(msg.sender, mintIndex);
+                emit PublicSaleMint(msg.sender, mintIndex);
+                _mintedItems++;
+            }}
+    }
+
+
+    // Get MetaData TokenURI =======================
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString()))
+        : "";
+    }
+
+
+    function setPresaleStartTimestamp(uint _startTimestamp) external onlyOwner {
+        presaleStartTimestamp = _startTimestamp;
+    }
+
+    function setPublicSaleStartTimestamp(uint _startTimestamp) external onlyOwner {
+        publicSaleStartTimestamp = _startTimestamp;
+    }
+
+    function setPresaleCost(uint256 _newCost) public onlyOwner {
+        presaleCost = _newCost;
+    }
+
+    function setPublicSaleCost(uint256 _newCost) public onlyOwner {
+        publicSaleCost = _newCost;
+    }
+
+    function setMaxMintAmount(uint256 _maxItemsPerTx) public onlyOwner {
+        maxMintAmount = _maxItemsPerTx;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function whitelistUser(address _user) public onlyOwner {
+        whitelisted[_user] = true;
+    }
+
+    function removeWhitelistUser(address _user) public onlyOwner {
+        whitelisted[_user] = false;
+    }
+
+    function presaleUser(address _user, uint _amount) internal {
+        presaleWallets[_user] = _amount;
+    }
+
+    function getMintedCountByPresaledUser(address _user) public view virtual returns (uint) {
+        return presaleWallets[_user];
+    }
+
+    function removePresaleUser(address _user, uint _amount) external onlyOwner {
+        presaleWallets[_user] = _amount;
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success,) = owner().call{value : address(this).balance}("");
+        require(success, "Failed to withdraw");
+    } 
+
+
+}
--- src/components/admin/AdminScreen.js
+++ src/components/admin/AdminScreen.js
@@ -1,11 +1,155 @@
 import React from 'react';
 
-const AdminScreen = ({accountAddress}) => {
+const AdminScreen = ({ accountAddress }) => {
     return <div className='container-fluid admin-wrapper'>
         <div className='container'>
             <div className='row d-block justify-content-center mt-5'>
                 <p className='heading1 text-center'>Owner Portal</p>
                 <span className='d-flex justify-content-center'><p className='font-weight-bold text-light'>Owner Address:</p><p className='text-light ml-3'>{accountAddress}</p></span>
+            </div>
+
+            <div className='row mt-5'>
+                <div className='col-7'>
+                    <div className='admin-options'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Pause Contract
+                            </button>
+                        </div>
+                        <div className='d-flex'>
+                            <p className='admin-txt1'>Current Status:</p>
+                            <p className='admin-txt2'>False</p>
+                        </div>
+                    </div>
+
+                    <div className='admin-options mt-4'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Set Base URI
+                            </button>
+                            <input type='text' className='input1' placeholder='Set your base uri' />
+                        </div>
+                        <div className='d-flex'>
+                            <p className='admin-txt1'>Current URI:</p>
+                            <p className='admin-txt2'>https://ipfs.infura.io/ipfs/</p>
+                        </div>
+                    </div>
+
+                    <div className='admin-options mt-4'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Set Presale Cost
+                            </button>
+                            <input type='number' className='input1' placeholder='Enter presale cost in ether' />
+                        </div>
+                        <div className='d-flex'>
+                            <p className='admin-txt1'>Current Cost:</p>
+                            <p className='admin-txt2'>0.02 ether</p>
+                        </div>
+                    </div>
+
+                    <div className='admin-options mt-4'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Set Public Sale Cost
+                            </button>
+                            <input type='number' className='input1' placeholder='Enter public sale cost in ether' />
+                        </div>
+                        <div className='d-flex'>
+                            <p className='admin-txt1'>Current Cost:</p>
+                            <p className='admin-txt2'>0.08 ether</p>
+                        </div>
+                    </div>
+
+
+                    <div className='admin-options mt-4'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Set Presale Time
+                            </button>
+                            <input type='number' className='input1' placeholder='Enter timestamp e.g 1649959200' />
+                        </div>
+                        <div className='d-flex'>
+                            <p className='admin-txt1'>Current Timestamp:</p>
+                            <p className='admin-txt2'>1649959200</p>
+                        </div>
+                    </div>
+
+                    <div className='admin-options mt-4'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Set Public sale Time
+                            </button>
+                            <input type='number' className='input1' placeholder='Enter timestamp e.g 1649959200' />
+                        </div>
+                        <div className='d-flex'>
+                            <p className='admin-txt1'>Current Timestamp:</p>
+                            <p className='admin-txt2'>1649959200</p>
+                        </div>
+                    </div>
+
+                    <div className='admin-options mt-4'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Add Whitelist Address
+                            </button>
+                            <input type='text' className='input1' placeholder='0xb221C202cF15E088B5DF9C60A19...' />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-5'>
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Contract:</p>
+                        <p className='admin-txt2'>0x1ceF4Eb1e2A398421073B349CA77D5FAF0d8AcB0</p>
+                    </div>
+
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Name:</p>
+                        <p className='admin-txt2'>Stubborn Apes</p>
+                    </div>
+
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Symbol:</p>
+                        <p className='admin-txt2'>APE</p>
+                    </div>
+
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Total Minted Tokens:</p>
+                        <p className='admin-txt2'>12</p>
+                    </div>
+
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Max Supply:</p>
+                        <p className='admin-txt2'>10000</p>
+                    </div>
+
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Presale Cost:</p>
+                        <p className='admin-txt2'>0.02 Ether</p>
+                    </div>
+
+
+                    <div className='d-flex'>
+                        <p className='admin-txt1'>Public Sale cose:</p>
+                        <p className='admin-txt2'>0.08 Ether</p>
+                    </div>
+
+                    <div className='d-flex mt-4'>
+                        <p className='admin-txt1'>Balance:</p>
+                        <p className='admin-txt2'>6.0503 Ether</p>
+                    </div>
+
+
+                    <div className='admin-options mt-2'>
+                        <div className='d-flex'>
+                            <button className='button5'>
+                                Withdraw
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </div>
